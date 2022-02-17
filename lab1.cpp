65ac33aeeab0a49006f133a6566c3207c727a23a
krssn <khanina.va@phystech.edu>
2022-02-17 17:00:33 +0300

    Add project files.

---
+++ lab1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#define n 5 //размер решетки
+#define m 3 //число дислокаций
+using namespace std;
+
+//генерация рандомных чисел
+int random(int k) {
+    srand(time(NULL));
+    int f = (rand() % m);
+    return f;
+}
+
+int main()
+{ 
+    int A[n][n] = {
+        { 1, 1, 1, 1 ,1},
+        { 1, 1, 1, 1 ,1},
+        { 1, 1, 1, 1 ,1},
+        { 1, 1, 1, 1 ,1},
+        { 1, 1, 1, 1 ,1} };
+
+    int x = random(n);
+    int y = random(n);
+    A[x][y] = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << A[i][j];
+        }
+        cout << "\n";
+    }
+    
+}
