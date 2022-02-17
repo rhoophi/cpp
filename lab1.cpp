d35180edb6b1a44802a2c33347bd59b375433d64
krssn <khanina.va@phystech.edu>
2022-02-17 19:41:47 +0300

    лаба.

--- lab1.cpp
+++ lab1.cpp
@@ -1,33 +1,60 @@
 ﻿#include <iostream>
-#define n 5 //размер решетки
+#include <fstream>
+#include <time.h>
+#define n 500 //размер решетки
 #define m 3 //число дислокаций
 using namespace std;
 
 //генерация рандомных чисел
 int random(int k) {
-    srand(time(NULL));
-    int f = (rand() % m);
+    float f = (rand() % n);
     return f;
 }
 
 int main()
 { 
-    int A[n][n] = {
-        { 1, 1, 1, 1 ,1},
-        { 1, 1, 1, 1 ,1},
-        { 1, 1, 1, 1 ,1},
-        { 1, 1, 1, 1 ,1},
-        { 1, 1, 1, 1 ,1} };
+    srand(time(NULL));
+    ofstream outf("task1.txt", ios::app);
+
+    int A[n][n];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            A[i][j] = 1;
+        }
+    }
 
     int x = random(n);
     int y = random(n);
     A[x][y] = 0;
-    
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cout << A[i][j];
         }
         cout << "\n";
     }
-    
+    bool end = 0;
+    for (int step = 0; !end; ++step) {
+        if ((x == 0) || (x == n - 1) || (y == 0) || (y == n - 1)) {
+            outf << n << " " << step << "\n";
+            end = 1;
+        }
+        else {
+            int v = random(4);
+            if (v == 0) {
+                x++;
+            }
+            if (v == 1) {
+                x--;
+            }
+            if (v == 2) {
+                y++;
+            }
+            if (v == 3) {
+                y--;
+            }
+        }
+    }
+
+
 }
